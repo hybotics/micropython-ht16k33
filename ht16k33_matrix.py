6ae6c7aa1dd5648a4058eae308bcf014ddd04d53
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2016-05-11 00:11:15 +0200

    Add library for 7seg and 14seg displays

--- ht16k33_matrix.py
+++ ht16k33_matrix.py
@@ -4,34 +4,35 @@ HT16K33_CMD_BRIGHTNESS = const(0xE0)
 HT16K33_OSCILATOR_ON = const(0x21)
 
 
-class Matrix:
+class HT16K33:
     def __init__(self, i2c, address=0x70):
         self.i2c = i2c
         self.address = address
-        self.temp = bytearray(1)
+        self._temp = bytearray(1)
         self.buffer = bytearray(16)
         self.fill(0)
-        self.write_cmd(HT16K33_OSCILATOR_ON)
+        self._write_cmd(HT16K33_OSCILATOR_ON)
         self.blink_rate(0)
         self.brightness(15)
 
-    def write_cmd(self, byte):
-        self.temp[0] = byte
-        self.i2c.writeto(self.address, self.temp)
+    def _write_cmd(self, byte):
+        self._temp[0] = byte
+        self.i2c.writeto(self.address, self._temp)
 
     def blink_rate(self, rate=None):
         if rate is None:
             return self._blink_rate
         rate = rate & 0x02
         self._blink_rate = rate
-        self.write_cmd(HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | rate << 1)
+        self._write_cmd(HT16K33_BLINK_CMD |
+                        HT16K33_BLINK_DISPLAYON | rate << 1)
 
     def brightness(self, brightness):
         if brightness is None:
             return self._brightness
         brightness = brightness & 0x0F
         self._brightness = brightness
-        self.write_cmd(HT16K33_CMD_BRIGHTNESS | brightness)
+        self._write_cmd(HT16K33_CMD_BRIGHTNESS | brightness)
 
     def show(self):
         self.i2c.writeto_mem(self.address, 0x00, self.buffer)
@@ -41,7 +42,7 @@ class Matrix:
         for i in range(16):
             self.buffer[i] = fill
 
-    def pixel(self, x, y, color=None):
+    def _pixel(self, x, y, color=None):
         mask = 1 << x
         if color is None:
             return bool((self.buffer[y] | self.buffer[y + 1] << 8) & mask)
@@ -53,20 +54,20 @@ class Matrix:
             self.buffer[y * 2 + 1] &= ~(mask >> 8)
 
 
-class Matrix8x16(Matrix):
+class Matrix8x16(HT16K33):
     def pixel(self, x, y, color=None):
         if not 0 <= x <= 15:
             return
         if not 0 <= y <= 8:
             return
-        return super().pixel(x, y, color)
+        return super()._pixel(x, y, color)
 
 
-class Matrix8x8(Matrix):
+class Matrix8x8(HT16K33):
     def pixel(self, x, y, color=None):
         if not 0 <= x <= 8:
             return
         if not 0 <= y <= 8:
             return
         x = (x - 1) % 8
-        return super().pixel(x, y, color)
+        return super()._pixel(x, y, color)
