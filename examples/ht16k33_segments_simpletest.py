cdf0b2fbaf1ea3dbbc9b778fcd7008d99320f079
Dale Weber <hybotics.wy@gmail.com>
2021-07-28 19:22:07 -0700

    These are the REAL modified files for Seg7x4 testing.

--- examples/ht16k33_segments_simpletest.py
+++ examples/ht16k33_segments_simpletest.py
@@ -10,7 +10,7 @@ from dotstar import DotStar
 # Import all board pins.
 from machine import SoftI2C, SoftSPI, Pin
 # Import the HT16K33 LED segment module.
-from hybotics_ht16k33.segments import Seg14x4       # Can also be Seg7x4, BigSeg7x4
+from hybotics_ht16k33.segments import Seg7x4, Seg14x4       # Can also be Seg7x4, BigSeg7x4
 # Import special stuff for tinyPico
 from tinypico import set_dotstar_power, dotstar_color_wheel, I2C_SDA, I2C_SCL, DOTSTAR_CLK, DOTSTAR_DATA, SPI_MISO
 from random import random
@@ -44,7 +44,7 @@ sleep_ms(DOTSTAR_OFF_MS)
 # This creates a 7 segment 4 character display:
 # display = Seg7x4(i2c)
 # Or this creates a 14 segment alphanumeric 4 character display:
-display = Seg14x4(i2c)
+display = Seg7x4(i2c, address=0x75)
 # Or this creates a big 7 segment 4 character display
 # display = BigSeg7x4(i2c)
 # Finally you can optionally specify a custom I2C address of the HT16k33 like:
@@ -59,16 +59,16 @@ try:
   sleep(DELAY_BETWEEN_SEC)
 
   # Can just print a floating point number
-  int_number = 33.14546
+  int_number = 3141
   print("Printing an integer number {0}".format(int_number))
   display.print(int_number, auto_round=True)
   sleep(DELAY_BETWEEN_SEC)
   display.fill(0)
 
   # Can just print a floating point number
-  float_number = 179.2545
+  float_number = 29.2545
   print("Printing a floating point number {0}".format(float_number))
-  display.print(float_number, decimal=1)
+  display.print(float_number, decimal=2)
   sleep(DELAY_BETWEEN_SEC)
   display.fill(0)
 
@@ -76,6 +76,12 @@ try:
   hex_number = 0xBEAD
   print("Printing a hexadecimal number {0}".format(hex(hex_number)))
   display.print_hex(hex_number)
+  sleep(DELAY_BETWEEN_SEC)
+  display.fill(0)
+
+  time_value = '10:22'
+  print("Printing a time of {0}".format(time_value))
+  display.print(time_value)
   sleep(DELAY_BETWEEN_SEC)
   display.fill(0)
 
@@ -92,18 +98,18 @@ try:
   display.fill(0)
 
   # Or, can even set the segments to make up characters
-  if isinstance(display, Seg14x4):
-      # 14-segment raw digits
-      display.set_digit_raw(0, 0x2D3F)
-      display.set_digit_raw(1, 0b0010110100111111)
-      display.set_digit_raw(2, (0b00101101, 0b00111111))
-      display.set_digit_raw(3, [0x2D, 0x3F])
-  else:
+  if isinstance(display, Seg7x4):
       # 7-segment raw digits
       display.set_digit_raw(0, 0xFF)
       display.set_digit_raw(1, 0b11111111)
       display.set_digit_raw(2, 0x79)
       display.set_digit_raw(3, 0b01111001)
+  else:
+      # 14-segment raw digits
+      display.set_digit_raw(0, 0x2D3F)
+      display.set_digit_raw(1, 0b0010110100111111)
+      display.set_digit_raw(2, (0b00101101, 0b00111111))
+      display.set_digit_raw(3, [0x2D, 0x3F])
 
   sleep(DELAY_BETWEEN_SEC)
   display.fill(0)
